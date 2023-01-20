e9efdbf4ecb356f1d0b6dff9d8d0b0e1fa0310d1
Tejora <numan.992@gmail.com>
2023-01-20 20:02:03 +0530

    Task commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MultiplicationTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 2 numbers : ");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = 0;
+            for(int i=0; i<num1; i++)
+            {
+                result += num2;
+            }
+            Console.WriteLine(num1 + " * " + num2 +" : " + result);
+
+
+        }
+    }
+}
